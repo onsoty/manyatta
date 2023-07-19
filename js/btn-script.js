7000def11b46ef7212f2fa85b2f4d8191aeb3e23
Daniel Omare <onsoty@gmail.com>
2023-07-20 02:35:40 +0300

    Added new files  to make the site look good on computer

---
+++ js/btn-script.js
@@ -0,0 +1,5 @@
+let btn = document.querySelector("button");
+
+btn.onclick = function () {
+  alert("You Clicked me!");
+};
---
+++ js/script.js
@@ -0,0 +1,12 @@
+// JavaScript Testpage
+document.write("This is a JavaScript Testpage");
+let age = 65;
+if (age <= 5) {
+  document.write("<p>Where is your mother</p>");
+} else if (age >= 65) {
+  document.write("<p>Budda, We mzee kalale</p>");
+} else if (age >= 18) {
+  document.write("<p>You are allowed to enter</p>");
+} else {
+  document.write("<p>You are to young, Go Home Dude.</p>");
+}
